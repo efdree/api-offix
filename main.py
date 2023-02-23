dca74e53638c20cf0814019b43575803ab0a8752
Erik Cardenas <efdree@gmail.com>
2023-02-23 18:58:26 -0500

    Add: CORS Middleware

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from config.database import engine, Base
 from middlewares.error_handler import ErrorHandler
 from routers.employee import employee_router
 from routers.department import department_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.title = "Offix API"
 app.version = "0.0.1"
 
-app.add_middleware(ErrorHandler)
+origins = [
+    "http://localhost:8000",
+    "http://localhost:3000"
+]
 
+app.add_middleware(ErrorHandler)
+app.add_middleware(CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+                   )
 app.include_router(employee_router)
 app.include_router(department_router)
 
